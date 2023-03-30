f13bdfa22d3d0655f889faf9777d46f2f916e285
NaimunNahar14 <naimunnaharshoshi@gmail.com>
2023-03-30 11:00:39 -0700

    header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+       <nav className='header'>
+        <h4>knowledge Site</h4>
+        <div>
+        <a href="">Blog</a>
+        </div>
+        
+       
+       </nav>
+        
+    );
+};
+
+export default Header;
